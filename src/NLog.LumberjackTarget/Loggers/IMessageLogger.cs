9b55a4a5acbf5fb8bbde1069fdf55574c3b9a388
zhekaby <zhekaby@gmail.com>
2015-08-25 12:04:04 +0300

    Revert "add fluent api"

--- src/NLog.LumberjackTarget/Loggers/IMessageLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NLog.Targets.Lumberjack.Loggers
-{
-    public interface IMessageLogger
-    {
-        void LogMessage(LumberjackLogMessage logMessage);
-        void LogAlert(LumberjackAlertMessage alertMessage);
-        void LogMeasure(LumberjackMetricMessage measureMessage);
-    }
-}
